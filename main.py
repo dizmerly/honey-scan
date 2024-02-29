5c4b48a936d1b2613a5296efee6d2b5e0cd905f0
dizmerly <dan.izmerly@gmail.com>
2024-02-28 19:39:09 -0600

    Library Imports

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+# Used to securely store your API key
+from google.colab import userdata
