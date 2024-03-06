a8c1ec160418f850d5221e78c44c81ff43612f19
dizmerly <dan.izmerly@gmail.com>
2024-03-06 10:20:34 -0600

    file updates

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import pathlib
-import textwrap
-
-import google.generativeai as genai
-
-from IPython.display import display
-from IPython.display import Markdown
+import requests
+from api_key import API_TOKEN
 
 
-def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+def call(prompt: str) -> str:
+    resp = requests.post(
+        'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' + API_TOKEN,
+        json={'contents': [{'parts': [{'text': prompt}]}]})
 
-# Used to securely store your API key
-from google.colab import userdata
+    if resp.status_code != 200:
+        return ''
+    return resp.json().get('candidates')[0].get('content').get('parts')[0].get('text')
+
+
+thought = call(
+    'Are you able to analyze images? Answer in under 300 characters')
+print('%d: %s' % (0, thought))
